58516f8d1bf90b19efe8ad122b4791c0a10e0fe0
krashishverma <kr.ashishverma@gmail.com>
2024-12-02 15:03:07 +0530

    deploy3

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
-const nextConfig = {
-  output: 'export',
-  basePath: '/MyPortfolio',
-  trailingSlash: true,
-};
-
-module.exports = nextConfig;
+module.exports = {
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
+  output: 'standalone', // This is an example of setting Next.js to static export if needed
+  basePath: '/MyPortfolio', // Set this if you want to deploy on GitHub Pages with a base path
+  trailingSlash: true, // Ensure URLs end with a slash
+}
